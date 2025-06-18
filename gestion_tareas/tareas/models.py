b62e6835e7ff62a59def999f5005e8fadcdf709b
cristianr02020 <cdromeroc3030@gmail.com>
2025-06-17 22:14:59 -0400

    Initial commit

---
+++ gestion_tareas/tareas/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Tarea(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    completado = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
---
+++ gestion_tareas/tareas/views.py
@@ -0,0 +1,7 @@
+from rest_framework import generics
+from .models import Tarea
+from .serializers import TareaSerializer
+
+class TareaListCreate(generics.ListCreateAPIView):
+    queryset = Tarea.objects.all()
+    serializer_class = TareaSerializer
